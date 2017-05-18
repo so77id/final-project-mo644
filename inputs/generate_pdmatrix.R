3667af93fcb05cfd92a52f65dc46c7e141fc2c11
Nathália Menini <nathmenini@gmail.com>
2017-05-17 22:54:57 -0300

    Update generate_pdmatrix.R

--- inputs/generate_pdmatrix.R
+++ inputs/generate_pdmatrix.R
@@ -1,16 +1,19 @@
 
+# enter with your path
 path = "/Users/almeida/Dropbox/Unicamp/Disciplinas/MO644/Trabalho/input/matriz_"
 
-k <- c(3, 5, 50, 100, 500, 1000) # variables
-n <- 10000 # observations
+k <- c(3, 5, 50, 100, 500, 1000) # dimensions
+n <- 10000 # number that we'll use in matrix simulation
 
 for(j in 1:length(k)){
 	
+	# guarantees that in every simulation the numbers generated are equals
 	set.seed(1)
 	
 	M <- NULL
 	M <- matrix(NA,n,k[j])
 	
+	# sampling
 	for(i in 1:k[j]){
 		M[,i] <- runif(n, 1, 50)
 	}
@@ -21,17 +24,22 @@ for(j in 1:length(k)){
 	#creates a difference matrix
 	D <- M - M_mean
 	
-	#creates the covariance matrix
-	C <- cov(D) # sample covariance
+	#creates the sample covariance matrix
+	C <- cov(D)
 	
+	# avoid floating numbers
 	C <- round(C)
 	
+	# TRUE: the matrix is positive definite
+	# FALSE: the simulation failed
 	print(is.positive.definite(C))
 	
+	# organizing the data with the dimension and the inputs of the matrix
 	m_ps <- rep(NA, k[j]^2 + 1)
 	m_ps[1] <- k[j]
 	m_ps[2:length(m_ps)] <- as.vector(C)
 	
+	# saving the .in file
 	write.table(rbind(m_ps), paste0(path, as.character(k[j]), "x", as.character(k[j]), ".in"), sep=" ", col.names = F, row.names = F)
 	
 }
