b4ecac2e02ba8f9ac4e74f5549eb6fba09970c3a
Nathália Menini <nathmenini@gmail.com>
2017-05-22 12:55:17 -0300

    Update cholesky_openmp.c

--- cholesky/cholesky_openmp.c
+++ cholesky/cholesky_openmp.c
@@ -53,6 +53,8 @@ int main() {
 	// Numero de threads
 	scanf("%d",&nt);
 	
+	nt=2; // mudar enquanto esta testando, depois colocamos como input junto no arquivo in
+	
 	// Dimensao da matriz
 	scanf("%d",&n);
 	
