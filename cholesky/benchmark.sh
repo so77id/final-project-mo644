7b9facdc057522e589fa00262fd93e33d6fdb19c
Miguel Rodriguez <m.rodriguezs1990@gmail.com>
2017-08-08 12:34:31 -0300

    finish commit for project

--- cholesky/benchmark.sh
+++ cholesky/benchmark.sh
@@ -1,5 +1,5 @@
 BINARY_FOLDER=binaries
-INPUT_FOLDER='../inputs'
+INPUT_FOLDER='./input'
 
 EXECUTABLE_SERIAL=cholesky_serial_for_parallel.bin
 EXECUTABLE_OPENMP=cholesky_openmp.bin
@@ -9,7 +9,7 @@ EXECUTABLE_PTHREAD_PRODCON=cholesky_pthreads_prodcon.bin
 
 ITERATIONS=1
 
-INPUTS=("$INPUT_FOLDER/matrix_3x3.in" "$INPUT_FOLDER/matrix_5x5.in" "$INPUT_FOLDER/matrix_50x50.in" "$INPUT_FOLDER/matrix_100x100.in" "$INPUT_FOLDER/matrix_500x500.in" "$INPUT_FOLDER/matrix_1000x1000.in" "$INPUT_FOLDER/matrix_2000x2000.in" "$INPUT_FOLDER/matrix_3000x3000.in" "$INPUT_FOLDER/matrix_5000x5000.in" "$INPUT_FOLDER/matrix_6000x6000.in" "$INPUT_FOLDER/matrix_7000x7000.in")
+INPUTS=("$INPUT_FOLDER/matriz_5x5.in" "$INPUT_FOLDER/matriz_50x50.in" "$INPUT_FOLDER/matriz_100x100.in" "$INPUT_FOLDER/matriz_500x500.in" "$INPUT_FOLDER/matriz_1000x1000.in")
 N_THREADS=(1 2 4 8 16 24 32)
 
 SERIAL_TIMES=()
